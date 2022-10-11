7f9033416c28fa7d327b0c35fb9c28efb6fe626a
mhenriette <mhenriette00@gnmail.com>
2022-10-11 08:59:46 +0200

    first setup of the project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./*html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                Roboto: ['Roboto']
+            },
+            colors: {
+                ourblue: '#1D2130',
+                lightviolet: '#E0E4FC',
+                lightrose: ' #D798E126',
+                lightblack: '#2B2E3C;',
+                bordercolor: '#D8D8D8',
+                indigocolor: '#6B7CFF',
+                darkwhite: '#f8fafd',
+
+            },
+        },
+    },
+    // plugins: [],
+    // safelist: [{
+    //     pattern: /.*/,
+    // }, ],
+}
